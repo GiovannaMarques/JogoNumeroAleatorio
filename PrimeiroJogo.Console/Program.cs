5d505f08d94d7ae44612d71f32e45977bc1ecb45
Matheus Campanini Mughrabi <matheus.mughrabi@gmail.com>
2021-11-02 20:41:07 -0300

    - Cria passos do programa para auxiliar a implementação

--- PrimeiroJogo.Console/Program.cs
+++ PrimeiroJogo.Console/Program.cs
@@ -6,10 +6,15 @@ namespace PrimeiroJogo.ConsoleUI
     {
         static void Main(string[] args)
         {
-            //Escrever uma variavel e atribuir valor
-            //Modificar o valor dessa variavel
-            int numero = 10;
-            numero = 14;
+            // Primeiramente, o jogo vai mostrar uma mensagem explicativa para o usuário
+
+            // Quando o usuário der 'enter', o jogo começa e cria um número aleatório
+            // Então o jogo pede para que o usuário digite um número
+            // Então, o programa verifica se o número digitado é igual ao número aleatório definido pelo programa
+            // Se o usuário acertou, o jogo dá os parabéns e se o usuário errou o jogo informa o erro
+            // Em seguida o jogo reinicia
         }
     }
 }
+
+// Próxima feature -> Criar mecanismo de chances que o usuário tem para acertar o número
