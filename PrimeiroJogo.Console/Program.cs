2deee401a84761aa27a7159ee4f0042f82aa2121
Giovanna Marques <giovannalimamarques@hotmail.com>
2021-11-02 20:30:23 -0300

    criado projeto

---
+++ PrimeiroJogo.Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PrimeiroJogo.ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Escrever uma variavel e atribuir valor
+            //Modificar o valor dessa variavel
+            int numero = 10;
+            numero = 14;
+        }
+    }
+}
