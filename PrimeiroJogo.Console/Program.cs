08fa51717a3690beeb7ed975adf4432153c1ce41
GiovannaMarques <giovannalimamarques@hotmail.com>
2021-11-08 20:50:41 -0300

    Primeira Versão Funcional Implementada

--- PrimeiroJogo.Console/Program.cs
+++ PrimeiroJogo.Console/Program.cs
@@ -7,21 +7,32 @@ namespace PrimeiroJogo.ConsoleUI
         static void Main(string[] args)
         {
             // Primeiramente, o jogo vai mostrar uma mensagem explicativa para o usuário
-
             // Quando o usuário der 'enter', o jogo começa e cria um número aleatório
             // Então o jogo pede para que o usuário digite um número
             // Então, o programa verifica se o número digitado é igual ao número aleatório definido pelo programa
             // Se o usuário acertou, o jogo dá os parabéns e se o usuário errou o jogo informa o erro
             // Em seguida o jogo reinicia
 
-            Console.WriteLine("Bem vindo ao Jogo Aleatorio!\nEscolha um número aleatório de 1 a 10");
-
-            int resposta = int.Parse(Console.ReadLine());
+            Console.WriteLine("Bem vindo ao Jogo Aleatorio!");
             Random random = new Random();
             int numero = random.Next(1, 10);
 
-
-
+            int parar = 1;
+            while (parar < 2)
+            {
+                Console.WriteLine("Escolha um número de 1 a 10");
+                int resposta = int.Parse(Console.ReadLine());
+                
+                if (numero == resposta)
+                {
+                    Console.WriteLine($"Parabéns você ganhou, o número era {numero}");
+                    parar = 30;
+                }
+                else
+                {
+                    Console.WriteLine($"Você errou. O número era {numero}");
+                }
+            }
         }
     }
 }
